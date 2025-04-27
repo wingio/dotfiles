7e91c9c2027a14ef7628d328b6a64019bb823a33
wingio <44992537+wingio@users.noreply.github.com>
2025-04-27 00:43:54 -0400

    Changes to shell and fastfetch
    - Added mini version of fastfetch that appears on shell init
    - Added terminal color dots to fastfetch
    - Use primarily octicons

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting ""
+
+ff -c ~/.config/fastfetch/mini.jsonc # Miniature fastfetch just for shell startup
 starship init fish | source
