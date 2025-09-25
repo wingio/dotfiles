bfc5aa2f1d9d40d4866f61ead5c0cc0d35bc30ef
Wing <44992537+wingio@users.noreply.github.com>
2025-09-25 16:10:34 -0400

    Enable transient prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting ""
+function starship_transient_prompt_func
+  starship module character
+end
 
+set fish_greeting ""
 ff -c ~/.config/fastfetch/mini.jsonc # Miniature fastfetch just for shell startup
+
 starship init fish | source
+enable_transience
