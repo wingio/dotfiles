bc7f3a35fa550a1fa2a28ead5041aa19e056ab89
Wing <git@iamw.ing>
2026-01-30 18:31:25 -0500

    Set up ssh-agent for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,9 @@ function starship_transient_prompt_func
   starship module character
 end
 
+setenv SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 set fish_greeting ""
+
 ff -c ~/.config/fastfetch/mini.jsonc # Miniature fastfetch just for shell startup
 
 starship init fish | source
